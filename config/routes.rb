cf9194f643d5646b6adb1454d4a9490815f673f3
amiblog <reza.am.0425@gmail.com>
2022-02-10 07:19:48 +0000

    投稿周り完了

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :books, only: [:new, :create, :index, :show, :edit, :update]
+  root to: 'homes#top'
+  get 'home/about' => "homes#about"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
